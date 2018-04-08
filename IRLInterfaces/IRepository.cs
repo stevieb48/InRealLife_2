76c96e49a2388e6d5e8ba34f300b5c003492f348
stevieb48 <seb48@students.uwf.edu>
2018-04-07 22:44:38 -0500

    Added empty IRepository

--- IRLInterfaces/IRepository.cs
+++ IRLInterfaces/IRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicLayer
+namespace IRLInterfaces
 {
-    public class Repository
+    public interface IRepository
     {
 
     }
