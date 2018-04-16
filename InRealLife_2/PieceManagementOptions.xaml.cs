14c60c553edcc0424a59f75669dab20da2fc8190
stevieb48 <seb48@students.uwf.edu>
2018-04-16 15:59:24 -0500

    added piece management options

---
+++ InRealLife_2/PieceManagementOptions.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Classes;
+using ClassInterfaces;
+using System.Windows;
+using System.Windows.Controls;
+
+/*
+ * This GUI ....
+ *
+ * author: Group 7 (Stephen Bailey, Omar Garcia, Craig Wyse, Matthew Harris)
+ * course: SEII
+ * assignment: InRealLife (Group Project Spring 2018)
+ * date: 04/05/2018
+ * file name: PieceManagementOptions.xaml.cs
+ * version: 1.0
+ */
+namespace InRealLife_2
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class PieceManagementOptions : Page
+    {
+        //
+        public PieceManagementOptions()
+        {
+            InitializeComponent();
+        }
+
+        //
+        private void BtnManageScenarios_Click(object sender, RoutedEventArgs e)
+        {
+            // call appropriate menu
+            //ScenarioMainMenu scenarioMainMenu = new ScenarioMainMenu();
+
+            // switch navigation to main menu
+            //scenarioMainMenu.Show();
+        }
+
+        //
+        private void BtnManageStages_Click(object sender, RoutedEventArgs e)
+        {
+            // call appropriate menu
+            //StageMainMenu StageMainMenu = new StageMainMenu();
+
+            // switch navigation to main menu
+            //StageMainMenu.Show();
+        }
+
+        //
+        private void BtnManageAnswers_Click(object sender, RoutedEventArgs e)
+        {
+            // call appropriate menu
+            //AnswerMainMenu answerMainMenu = new AnswerMainMenu();
+
+            // switch navigation to main menu
+            //answerMainMenu.Show();
+        }
+
+        //
+        private void BtnExitManagement_Click(object sender, RoutedEventArgs e)
+        {
+            //TitleScreen titleScreen = new TitleScreen();
+            //this.NavigationService.Navigate(titleScreen);
+        }
+    }
+}
