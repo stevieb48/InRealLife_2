49dc66fe27498d183d27e58ce193c9cb9249d6c2
stevieb48 <seb48@students.uwf.edu>
2018-04-16 15:05:29 -0500

    added_Create_NewOverallPage_needs_more_coding

---
+++ InRealLife_2/CreateNewOverall.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InRealLife_2
+{
+    /// <summary>
+    /// Interaction logic for CreateNewOverall.xaml
+    /// </summary>
+    public partial class CreateNewOverall : Page
+    {
+        public CreateNewOverall()
+        {
+            InitializeComponent();
+        }
+
+        // initialize the form
+        private void InitializeForm()
+        {
+            // load textboxes with information
+
+            // enable buttons
+
+        }
+
+        //
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // update scenario table with textbox data
+
+        }
+
+        //
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // call button exit click
+
+        }
+
+        //
+        private void btnExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // exit to main menu
+        }
+    }
+}
