67456c483e2827686ddfed55c92691e78c60c84e
stevieb48 <seb48@students.uwf.edu>
2018-04-18 14:31:20 -0500

    fixed tests for stage

--- Classes_Tests/Stage_Unit_Tests.cs
+++ Classes_Tests/Stage_Unit_Tests.cs
@@ -102,9 +102,9 @@ namespace Classes_Tests
             string audioFilePath = string.Empty;
             string imageFilePath = string.Empty;
             int answer1ID = 0;
-            int Ans1NextStagID = 0;
-            int Answer2ID = 0;
-            int Ans2NextStagID = 0;
+            int ans1NextStagID = 0;
+            int answer2ID = 0;
+            int ans2NextStagID = 0;
 
             Stage sut;
 
@@ -118,10 +118,10 @@ namespace Classes_Tests
             Assert.AreEqual(scenarioID, sut.ScenarioID, "Stage public ScenarioID Accessor method is working properly.");
             Assert.AreEqual(audioFilePath, sut.AudioFilePath, "Stage public AudioFilePath Accessor method is working properly.");
             Assert.AreEqual(imageFilePath, sut.ImageFilePath, "Stage public ImageFilePath Accessor method is working properly.");
-            //
-            //
-            //
-            //
+            Assert.AreEqual(answer1ID, sut.Answer1ID, "Stage public Answer1ID Accessor method is working properly.");
+            Assert.AreEqual(ans1NextStagID, sut.Ans1NextStagID, "Stage public Ans1NextStagID Accessor method is working properly.");
+            Assert.AreEqual(answer2ID, sut.Answer2ID, "Stage public Answer2ID Accessor method is working properly.");
+            Assert.AreEqual(ans2NextStagID, sut.Ans2NextStagID, "Stage public Ans2NextStagID Accessor method is working properly.");
         }
 
         [TestMethod]
@@ -151,10 +151,10 @@ namespace Classes_Tests
             Assert.AreEqual(1, sut.ScenarioID, "Stage public ScenarioID Accessor method is working properly.");
             Assert.AreEqual("C:\\Users\\parent\\audio\\audiofile1", sut.AudioFilePath, "Stage public AudioFilePath Accessor method is working properly.");
             Assert.AreEqual("C:\\Users\\parent\\image\\imagefile1", sut.ImageFilePath, "Stage public ImageFilePath Accessor method is working properly.");
-            //
-            //
-            //
-            //
+            Assert.AreEqual(1, sut.Answer1ID, "Stage public Answer1ID Accessor method is working properly.");
+            Assert.AreEqual(4, sut.Ans1NextStagID, "Stage public Ans1NextStagID Accessor method is working properly.");
+            Assert.AreEqual(2, sut.Answer2ID, "Stage public Answer2ID Accessor method is working properly.");
+            Assert.AreEqual(5, sut.Ans2NextStagID, "Stage public Ans2NextStagID Accessor method is working properly.");
         }
 
         [TestMethod]
@@ -168,9 +168,9 @@ namespace Classes_Tests
             string audioFilePath = "C:\\Users\\parent\\audio\\audiofile1";
             string imageFilePath = "C:\\Users\\parent\\image\\imagefile1";
             int answer1ID = 1;
-            int Ans1NextStagID = 4;
-            int Answer2ID = 2;
-            int Ans2NextStagID = 5;
+            int ans1NextStagID = 4;
+            int answer2ID = 2;
+            int ans2NextStagID = 5;
 
             Stage sut;
 
@@ -182,10 +182,10 @@ namespace Classes_Tests
             sut.ScenarioID = scenarioID;
             sut.AudioFilePath = audioFilePath;
             sut.ImageFilePath = imageFilePath;
-            //
-            //
-            //
-            //
+            sut.Answer1ID = answer1ID;
+            sut.Ans1NextStagID = ans1NextStagID;
+            sut.Answer2ID = answer2ID;
+            sut.Ans2NextStagID = ans2NextStagID;
 
             //Assert
             Assert.AreEqual(5, sut.ID, "Stage public ID Accessor method is working properly.");
@@ -194,10 +194,10 @@ namespace Classes_Tests
             Assert.AreEqual(1, sut.ScenarioID, "Stage public ScenarioID Accessor method is working properly.");
             Assert.AreEqual("C:\\Users\\parent\\audio\\audiofile1", sut.AudioFilePath, "Stage public AudioFilePath Accessor method is working properly.");
             Assert.AreEqual("C:\\Users\\parent\\image\\imagefile1", sut.ImageFilePath, "Stage public ImageFilePath Accessor method is working properly.");
-            //
-            //
-            //
-            //
+            Assert.AreEqual(1, sut.Answer1ID, "Stage public Answer1ID Accessor method is working properly.");
+            Assert.AreEqual(4, sut.Ans1NextStagID, "Stage public Ans1NextStagID Accessor method is working properly.");
+            Assert.AreEqual(2, sut.Answer2ID, "Stage public Answer2ID Accessor method is working properly.");
+            Assert.AreEqual(5, sut.Ans2NextStagID, "Stage public Ans2NextStagID Accessor method is working properly.");
         }
 
         [TestMethod]
@@ -225,22 +225,22 @@ namespace Classes_Tests
             sut.ScenarioID = 2;
             sut.AudioFilePath = "C:\\Users\\parent\\audio\\audiofile2";
             sut.ImageFilePath = "C:\\Users\\parent\\image\\imagefile3";
-            //
-            //
-            //
-            //
+            sut.Answer1ID = 10;
+            sut.Ans1NextStagID = 15;
+            sut.Answer2ID = 20;
+            sut.Ans2NextStagID = 25;
 
             //Assert
-            Assert.AreEqual(9, sut.ID, "Stage public ID Accessor method is working properly.");
-            Assert.AreEqual("Turn Up radio or turn down radio", sut.Name, "Stage public Name Accessor method is working properly.");
-            Assert.AreEqual("Turn up radio and ignor or turn down radio and listen to vehicle", sut.Description, "Stage public Description Accessor method is working properly.");
-            Assert.AreEqual(2, sut.ScenarioID, "Stage public ScenarioID Accessor method is working properly.");
-            Assert.AreEqual("C:\\Users\\parent\\audio\\audiofile2", sut.AudioFilePath, "Stage public AudioFilePath Accessor method is working properly.");
-            Assert.AreEqual("C:\\Users\\parent\\image\\imagefile3", sut.ImageFilePath, "Stage public ImageFilePath Accessor method is working properly.");
-            //
-            //
-            //
-            //
+            Assert.AreEqual(9, sut.ID, "Stage public ID Mutator method is working properly.");
+            Assert.AreEqual("Turn Up radio or turn down radio", sut.Name, "Stage public Name Mutator method is working properly.");
+            Assert.AreEqual("Turn up radio and ignor or turn down radio and listen to vehicle", sut.Description, "Stage public Description Mutator method is working properly.");
+            Assert.AreEqual(2, sut.ScenarioID, "Stage public ScenarioID Mutator method is working properly.");
+            Assert.AreEqual("C:\\Users\\parent\\audio\\audiofile2", sut.AudioFilePath, "Stage public AudioFilePath Mutator method is working properly.");
+            Assert.AreEqual("C:\\Users\\parent\\image\\imagefile3", sut.ImageFilePath, "Stage public ImageFilePath Mutator method is working properly.");
+            Assert.AreEqual(10, sut.Answer1ID, "Stage public Answer1ID Mutator method is working properly.");
+            Assert.AreEqual(15, sut.Ans1NextStagID, "Stage public Ans1NextStagID Mutator method is working properly.");
+            Assert.AreEqual(20, sut.Answer2ID, "Stage public Answer2ID Mutator method is working properly.");
+            Assert.AreEqual(25, sut.Ans2NextStagID, "Stage public Ans2NextStagID Mutator method is working properly.");
         }
     }
 }
