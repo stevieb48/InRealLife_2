ef3d9dc8dd7f7377e2a5ba018650ae9418e61931
wysecw <cww22@students.uwf.edu>
2018-04-22 19:41:54 -0500

    added comment instructions  to running.cs

--- InRealLife_2/Running.xaml.cs
+++ InRealLife_2/Running.xaml.cs
@@ -56,11 +56,13 @@ namespace InRealLife_2
         {
             InitializeComponent();
             Start(Scenario);
-
+            
+            //***********************imageFileName needs to be changed from hard coded value to database value i.e currentStage.fileName*************************** 
             string imageFileName = "flat.tire.10.jpg";
             string imageFilePath = System.IO.Path.Combine(currentDirectory, "mediaFiles", imageFileName);
             ImageBlock.Source = new BitmapImage(new Uri(imageFilePath, UriKind.RelativeOrAbsolute));
 
+            //****************soundFileName needs to be changed from hard coded value to database value i.e currentStage.fileName************************* 
             string soundFileName = "car_brake_crash.wav";
             string soundFilePath = System.IO.Path.Combine(currentDirectory, "mediaFiles", soundFileName);
             soundFX.Open(new Uri(soundFilePath, UriKind.RelativeOrAbsolute));
